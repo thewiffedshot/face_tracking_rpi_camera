c2e442a8eab75a2375fde7f6dcfb2e8337746977
thewiffedshot <thewiffedshot@gmail.com>
2021-09-01 01:53:23 +0300

    commit

--- camera_preview/face_detection/face_recognition.py
+++ camera_preview/face_detection/face_recognition.py
@@ -3,37 +3,71 @@
 import cv2 #For Image processing 
 import numpy as np #For converting Images to Numerical array 
 import os #To handle directories 
+import time
 from PIL import Image #Pillow lib for handling images 
+from servo_controller import ServoControl
+import functools
 
 labels = ["Simeon"] 
 
 face_cascade = cv2.CascadeClassifier('face_detection/haarcascade_frontalface_default.xml')
+servos = ServoControl()
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("face_detection/face-trainer.yml")
+deadzone_threshold_percentage = .1
+rotation_velocity = 6 # Degrees per second
+prev_time = 0
+target_vector = (0, 0)
 
 cap = cv2.VideoCapture(0) #Get video feed from the Camera
+frame_centerpoint = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) / 2, int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) / 2)
+position_centerpoint = frame_centerpoint
+
+def look_at(vector, delta):
+	if vector[0] < 0:
+		servos.moveYawDegrees(rotation_velocity * delta)
+	else:
+		servos.moveYawDegrees(-rotation_velocity * delta)
+
+	if vector[1] < 0:
+		servos.movePitchDegrees(rotation_velocity * delta)
+	else:
+		servos.movePitchDegrees(-rotation_velocity * delta)
 
 while(True):
 
-    ret, img = cap.read() # Break video into frames 
-    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert Video frame to Greyscale
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5) #Recog. faces
-    for (x, y, w, h) in faces:
-    	roi_gray = gray[y:y+h, x:x+w] #Convert Face to greys
+	ret, img = cap.read() # Break video into frames 
+	gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert Video frame to Greyscale
+	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5) #Recog. faces
 
-    	id_, conf = recognizer.predict(roi_gray) #recognize the Face
-    
-    	if conf >= 60:
-    		font = cv2.FONT_HERSHEY_SIMPLEX #Font style for the name 
-    		name = labels[id_] #Get the name from the List using ID number 
-    		cv2.putText(img, name, (x,y), font, 1, (0,0,255), 2)
-    	
-    	cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+	if len(faces) > 0:
+		face_positions = map(lambda x : (x[0] + x[2] / 2, x[1] + x[3] / 2), faces)
+		position_centerpoint = functools.reduce(lambda x, y : ((x[0] + y[0]) / 2, (x[1] + y[1]) / 2), face_positions)
+		target_vector = (position_centerpoint[0] - frame_centerpoint[0], position_centerpoint[1] - frame_centerpoint[1])
 
-    cv2.imshow('Preview',img) #Display the Video
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-    	break
+	if len(faces) > 0 and (abs(target_vector[0]) > deadzone_threshold_percentage * frame_centerpoint[0] or abs(target_vector[1]) > deadzone_threshold_percentage * frame_centerpoint[1] or target_vector == (0, 0)):
+		print(target_vector)
+		look_at(target_vector, time.time() - prev_time)
+
+	prev_time = time.time()
+
+	for (x, y, w, h) in faces:
+		roi_gray = gray[y:y+h, x:x+w] #Convert Face to greys
+
+		id_, conf = recognizer.predict(roi_gray) #recognize the Face
+	
+		if conf >= 60:
+			font = cv2.FONT_HERSHEY_SIMPLEX #Font style for the name 
+			name = labels[id_] #Get the name from the List using ID number 
+			cv2.putText(img, name, (x,y), font, 1, (0,0,255), 2)
+		
+		cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+	cv2.imshow('Preview',img) #Display the Video
+	if cv2.waitKey(20) & 0xFF == ord('q'):
+		break
 
 # When everything done, release the capture
 cap.release()
+servos.destroy()
 cv2.destroyAllWindows()
--- camera_preview/face_detection/servo_controller.py
+++ camera_preview/face_detection/servo_controller.py
@@ -2,52 +2,51 @@ import numpy as np
 import pigpio
 
 class ServoControl:
-    pwmFrequency = 50
-    servoGpioPin1 = 13
-    servoGpioPin2 = 12
-    currentYawAngle: int = 0 
-    currentPitchAngle: int = 0 
+    def __init__(self):
+        self.pwmFrequency = 50
+        self.servoGpioPin1 = 13
+        self.servoGpioPin2 = 12
+        self.currentYawAngle: int = 0 
+        self.currentPitchAngle: int = 0 
 
-    maxYawAngle = 60
-    maxPitchAngle = 45
+        self.maxYawAngle = 60
+        self.maxPitchAngle = 45
 
-    def __init__():
         global pwm
         pwm = pigpio.pi()
-        pwm.set_mode(ServoControl.servoGpioPin1, pigpio.OUTPUT)
-        pwm.set_PWM_frequency(ServoControl.servoGpioPin1, 330)
+        pwm.set_mode(self.servoGpioPin1, pigpio.OUTPUT)
+        pwm.set_PWM_frequency(self.servoGpioPin1, 330)
 
-        pwm.set_mode(ServoControl.servoGpioPin2, pigpio.OUTPUT)
-        pwm.set_PWM_frequency(ServoControl.servoGpioPin2, 330)
+        pwm.set_mode(self.servoGpioPin2, pigpio.OUTPUT)
+        pwm.set_PWM_frequency(self.servoGpioPin2, 330)
 
-    def moveYawDegrees(degrees: int):
-        ServoControl.currentYawAngle += degrees
+    def moveYawDegrees(self, degrees: int):
+        self.currentYawAngle += degrees
 
-        if (ServoControl.currentYawAngle > ServoControl.maxYawAngle):
-            ServoControl.currentYawAngle = ServoControl.maxYawAngle
-        elif (ServoControl.currentYawAngle < -ServoControl.maxYawAngle):
-            ServoControl.currentYawAngle = -ServoControl.maxYawAngle
+        if (self.currentYawAngle > self.maxYawAngle):
+            self.currentYawAngle = self.maxYawAngle
+        elif (self.currentYawAngle < -self.maxYawAngle):
+            self.currentYawAngle = -self.maxYawAngle
+            
+        pulseWidth = 1000 / 90 * self.currentYawAngle + 1500
+
+        pwm.set_servo_pulsewidth(self.servoGpioPin1, pulseWidth)
+
+    def movePitchDegrees(self, degrees: int):
+        self.currentPitchAngle += degrees
+
+        if (self.currentPitchAngle > self.maxPitchAngle):
+            self.currentPitchAngle = self.maxPitchAngle
+        elif (self.currentPitchAngle < -self.maxPitchAngle):
+            self.currentPitchAngle = -self.maxPitchAngle
             
 
-        pulseWidth = 1000 / 90 * ServoControl.currentYawAngle + 1500
+        pulseWidth = 1000 / 90 * self.currentPitchAngle + 1500
 
-        pwm.set_servo_pulsewidth(ServoControl.servoGpioPin1, pulseWidth)
+        pwm.set_servo_pulsewidth(self.servoGpioPin2, pulseWidth)
 
-    def movePitchDegrees(degrees: int):
-        ServoControl.currentPitchAngle += degrees
-
-        if (ServoControl.currentPitchAngle > ServoControl.maxPitchAngle):
-            ServoControl.currentPitchAngle = ServoControl.maxPitchAngle
-        elif (ServoControl.currentPitchAngle < -ServoControl.maxPitchAngle):
-            ServoControl.currentPitchAngle = -ServoControl.maxPitchAngle
-            
-
-        pulseWidth = 1000 / 90 * ServoControl.currentPitchAngle + 1500
-
-        pwm.set_servo_pulsewidth(ServoControl.servoGpioPin2, pulseWidth)
-
-    def destroy():
-        pwm.set_PWM_dutycycle(ServoControl.servoGpioPin1, 0)
-        pwm.set_PWM_frequency(ServoControl.servoGpioPin1, 0)
-        pwm.set_PWM_dutycycle(ServoControl.servoGpioPin2, 0)
-        pwm.set_PWM_frequency(ServoControl.servoGpioPin2, 0)
+    def destroy(self):
+        pwm.set_PWM_dutycycle(self.servoGpioPin1, 0)
+        pwm.set_PWM_frequency(self.servoGpioPin1, 0)
+        pwm.set_PWM_dutycycle(self.servoGpioPin2, 0)
+        pwm.set_PWM_frequency(self.servoGpioPin2, 0)
